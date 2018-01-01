97bd404aa825e3c4d39feb0222a31979d34a6259
Ziem0 <andrzejewski.ziemo@gmail.com>
2018-01-01 18:09:02 +0100

    add all regex

--- src/controller/InputController.java
+++ src/controller/InputController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import model.Hangman;
+
 public class InputController {
 
     public static boolean nameIsValid(String name) {
@@ -10,5 +12,19 @@ public class InputController {
         return level.matches("[1-3]");
     }
 
+    public static boolean letterIsValid(String letter) {
+        return letter.matches("[A-Z]{1}");
+    }
 
+    public static boolean actionIsValid(String action) {
+        return action.matches("[1-2]{1}");
+    }
+
+    public static boolean wordIsValid(String word) {
+        return word.matches("[A-Z]{1,}");
+    }
+
+    public static boolean finalAnswer(String capital, String answer) {
+        return capital.equals(answer);
+    }
 }
