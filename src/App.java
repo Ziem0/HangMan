98d83ae21b8bc07a1d5639ddc8e93b5c8c902760
Ziem0 <andrzejewski.ziemo@gmail.com>
2018-01-01 18:45:01 +0100

    clean

--- src/App.java
+++ src/App.java
@@ -110,8 +110,6 @@ public class App {
         }
     }
 
-
-
     public static void main(String[] args) {
         if (args.length > 0 && args[0].equals("cheat")) {
             isCheat = true;
--- src/view/UI.java
+++ src/view/UI.java
@@ -30,6 +30,7 @@ public class UI {
     }
 
     public static void clear() {
+        System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
